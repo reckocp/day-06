34cd4a79450e13da03951d78d9c44ffd656265cf
COLIN <reckocp@gmail.com>
2016-03-07 18:33:40 -0600

    Bouncer program Explorer Mode

--- bouncer.rb
+++ bouncer.rb
@@ -1,17 +1,26 @@
 def check_age
+    loop do
     puts "How old are you?"
     print "> "
-    user_age = gets.chomp.to_i
-      case user_age
+    user_age = gets.chomp
+    break if user_age.to_str == "stop"
+      case user_age.to_i
           when 0..17
             puts "You can't do anything."
+            puts "Type 'stop' to break out of this loop."
           when 18..20
             puts "You can vote."
             puts "You can smoke."
+            puts "Type 'stop' to break out of this loop."
           when 21..24
             puts "You can drink."
+            puts "Type 'stop' to break out of this loop."
           when 25..100
             puts "You can rent a car."
+            puts "Type 'stop' to break out of this loop."
+          when "stop"
+            break
+          end
         end
 end
 
